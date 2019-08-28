6eaaf4a53ee72ee0bb07eead883dc1fcbb4f2124
Mauro Herrera <mauherrera@bgeneral.com>
2019-08-27 21:11:16 -0500

    colors changed

--- nsolar-module-api/src/main/java/pa/nsolar/backend/module/services/dto/EnumModuleColor.java
+++ nsolar-module-api/src/main/java/pa/nsolar/backend/module/services/dto/EnumModuleColor.java
@@ -3,9 +3,9 @@ package pa.nsolar.backend.module.services.dto;
 public enum EnumModuleColor {
 
 	C1(100, 86, "rgb(58,168,243)"),
-	C2(85, 71, "rgb(127,198,160)"),
+	C2(85, 71, "rgb(127,160,198)"),
 	C3(70, 56, "rgb(22,139,228)"),
-	C4(55, 31, "rgb(19,101,66)"),
+	C4(55, 31, "rgb(19,101,162)"),
 	C5(30, 16, "rgb(20,84,128)"),
 	C6(15, 1, "rgb(17,45,62)"),
 	C7(0, 0, "rgb(0,0,0)");
